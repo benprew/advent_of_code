4a0c02e32d7c0672ae8f73d1bf4dad10b19dd2d5
Ben Prew <ben@throwingbones.com>
2024-12-13 21:34:57 -0800

    Day 8, part 2

--- 2024/8/solution.go
+++ 2024/8/solution.go
@@ -10,6 +10,10 @@ import (
 // start 9:35pm
 // finish 10:10pm
 
+// Part 2
+// start 9:15pm
+// finish 9:25pm
+
 type Point struct {
 	x, y int
 }
@@ -25,7 +29,7 @@ func main() {
 	// you need to iterate 2^k times.
 	// You can generate the binary string using Sprintf "%0*b"
 	fmt.Println("Part 1:", solve(filename))
-	// fmt.Println("Part 2:", solve(filename, []int{0, 1, 2}))
+	fmt.Println("Part 2:", solve2(filename))
 }
 
 // return the number of antinodes
@@ -56,6 +60,53 @@ func solve(filename string) (numAntinodes int) {
 					}
 					grid[anode.y][anode.x] = '#'
 				}
+			}
+		}
+	}
+	fmt.Println(antennas)
+	printGrid(grid)
+	return
+}
+
+func solve2(filename string) (numAntinodes int) {
+	grid, antennas := parse(filename)
+	// iterate through the antennas, calculate the distance between them and then
+	// check if that distance is again is still inside the grid
+	// if it is, we've found an antinode
+	for k, v := range antennas {
+		for i := 0; i < len(v); i++ {
+			for j := i + 1; j < len(v); j++ {
+				rise, run := pointsDist(v[i], v[j])
+
+				anode := Point{v[i].x + run, v[i].y + rise}
+				for anode.y >= 0 && anode.y < len(grid) && anode.x >= 0 && anode.x < len(grid[0]) {
+					fmt.Println(string(k), "antinode at", v[i].y+rise, v[i].x+run)
+					if grid[anode.y][anode.x] != '#' {
+						numAntinodes++
+					}
+					grid[anode.y][anode.x] = '#'
+					anode.x += run
+					anode.y += rise
+
+				}
+				anode = Point{v[j].x - run, v[j].y - rise}
+				for anode.y >= 0 && anode.y < len(grid) && anode.x >= 0 && anode.x < len(grid[0]) {
+					fmt.Println(string(k), "antinode at", v[i].y+rise, v[i].x+run)
+					if grid[anode.y][anode.x] != '#' {
+						numAntinodes++
+					}
+					grid[anode.y][anode.x] = '#'
+					anode.x -= run
+					anode.y -= rise
+				}
+			}
+		}
+	}
+	// also include all non '#' and non '.' squares
+	for _, row := range grid {
+		for _, c := range row {
+			if c != '#' && c != '.' {
+				numAntinodes++
 			}
 		}
 	}
