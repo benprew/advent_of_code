05dbf0bb0a3d35583a067f2bd2d5c1f69d3940fb
Ben Prew <ben@throwingbones.com>
2025-12-06 23:51:26 -0800

    2025 Day 4 - Part 1&2

---
+++ 2025/4/solution.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	filename := "input.txt"
+	if len(os.Args) > 1 {
+		filename = os.Args[1]
+	}
+	fmt.Println(filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	grid := parse(file)
+
+	fmt.Println("Part 1:", solve(grid))
+	total2 := 0
+	for {
+		t := solve2(grid)
+		if t == 0 {
+			break
+		}
+		total2 += t
+	}
+	fmt.Println("Part 2:", total2)
+}
+
+func solve(grid []string) (total int) {
+	for y, line := range grid {
+		for x, chr := range line {
+			if chr != '@' {
+				continue
+			}
+			Xrange := []int{-1, 0, 1}
+			Yrange := []int{-1, 0, 1}
+			bales := 0
+			for _, xr := range Xrange {
+				for _, yr := range Yrange {
+					if xr == yr && yr == 0 {
+						continue
+					}
+					// if x == 9 && y == 1 && y+yr >= 0 && x+xr >= 0 && y+yr < len(grid) && x+xr < len(line) {
+					// 	fmt.Println(xr, yr, string(grid[y+yr][x+xr]))
+					// }
+
+					if y+yr >= 0 && x+xr >= 0 && y+yr < len(grid) && x+xr < len(line) && grid[y+yr][x+xr] == '@' {
+						bales++
+					}
+				}
+			}
+			if bales < 4 {
+				fmt.Println(x, y, bales)
+				total++
+			}
+		}
+	}
+	return total
+}
+
+func solve2(grid []string) (total int) {
+	for y, line := range grid {
+		for x, chr := range line {
+			if chr != '@' {
+				continue
+			}
+			Xrange := []int{-1, 0, 1}
+			Yrange := []int{-1, 0, 1}
+			bales := 0
+			for _, xr := range Xrange {
+				for _, yr := range Yrange {
+					if xr == yr && yr == 0 {
+						continue
+					}
+					// if x == 9 && y == 1 && y+yr >= 0 && x+xr >= 0 && y+yr < len(grid) && x+xr < len(line) {
+					// 	fmt.Println(xr, yr, string(grid[y+yr][x+xr]))
+					// }
+
+					if y+yr >= 0 && x+xr >= 0 && y+yr < len(grid) && x+xr < len(line) && grid[y+yr][x+xr] == '@' {
+						bales++
+					}
+				}
+			}
+			if bales < 4 {
+				// fmt.Println(x, y, bales)
+				s := grid[y]
+				s = s[:x] + "x" + s[x+1:]
+				grid[y] = s
+				total++
+			}
+		}
+	}
+	return
+}
+
+func parse(file io.Reader) (lines []string) {
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	return
+}
